9b6621b73e629a3a38bea49017f09c8f7adad1c9
zhedlund <zhedlund@student.42berlin.de>
2024-03-21 19:47:58 +0100

    norminette get_cmd

--- march21_minishell/parsing/get_cmd.c
+++ march21_minishell/parsing/get_cmd.c
@@ -6,7 +6,7 @@
 /*   By: zhedlund <zhedlund@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 15:02:34 by zhedlund          #+#    #+#             */
-/*   Updated: 2024/03/21 13:54:01 by zhedlund         ###   ########.fr       */
+/*   Updated: 2024/03/21 19:46:43 by zhedlund         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,24 @@ void	handle_signals(t_info **info)
 	}
 }
 
+static int	handle_dot(char *buf, t_info **info)
+{
+	if (buf[1] == '/' && buf[2])
+		return (0);
+	else if (!buf[1])
+	{
+		printf("minishell: .: filename argument required\n");
+		printf(".: usage: filename [arguments]\n");
+		(*info)->exitstatus = 2;
+	}
+	else
+	{
+		printf("%s: command not found\n", buf);
+		(*info)->exitstatus = 127;
+	}
+	return (1);
+}
+
 static int	input_error(char *buf, t_info **info)
 {
 	if (buf[0] == '\0' || is_whitespace(buf))
@@ -38,30 +56,14 @@ static int	input_error(char *buf, t_info **info)
 	if ((buf[0] == '/' && !buf[1])
 		|| (buf[0] == '.' && buf[1] == '/' && !buf[2]))
 	{
-		printf("minishell: /: Is a directory\n");
+		printf("minishell: %s: Is a directory\n", buf);
 		(*info)->exitstatus = 126;
 		return (1);
 	}
 	if (buf[0] == '.')
-	{
-		if (buf[1] == '/' && buf[2])
-			return (0);
-		else if (!buf[1])
-		{
-			printf("minishell: .: filename argument required\n");
-			printf(": usage: filename [arguments]\n");
-			(*info)->exitstatus = 2;
-		}
-		else
-		{
-			printf("%s: command not found\n", buf);
-			(*info)->exitstatus = 127;
-		}
-		return (1);
-	}
+		return (handle_dot(buf, info));
 	if (ft_identical(buf, "\"\""))
 	{
-		printf("Command '' not found\n");
 		(*info)->exitstatus = 127;
 		return (1);
 	}
@@ -73,29 +75,29 @@ static int	input_error(char *buf, t_info **info)
  * return: 0 if input is not empty, -1 otherwise
  * note: the function is called by: main()
  */
-int get_cmd(char *buf, int buf_size, t_env **head, t_info **info)
+int	get_cmd(char *buf, int buf_size, t_env **head, t_info **info)
 {
-    char    *input;
-	
-    signal(SIGINT, ft_ctrlc);
-    signal(SIGQUIT, ft_ctrlc);
-    handle_signals(info);
-    if (isatty(0))
-        input = readline("minishell> ");
-    else
-        input = readline("");
-    if (input == NULL)
-    {
-        ft_putstr_fd("exit\n", 1);
-        ft_freelist(head);
-        free(*info);
-        exit(0);
-    }
-    ft_strlcpy(buf, input, buf_size);
-    buf[buf_size - 1] = '\0';
-    add_history(buf);
+	char	*input;
+
+	signal(SIGINT, ft_ctrlc);
+	signal(SIGQUIT, ft_ctrlc);
+	handle_signals(info);
+	if (isatty(0))
+		input = readline("minishell> ");
+	else
+		input = readline("");
+	if (input == NULL)
+	{
+		ft_putstr_fd("exit\n", 1);
+		ft_freelist(head);
+		free(*info);
+		exit(0);
+	}
+	ft_strlcpy(buf, input, buf_size);
+	buf[buf_size - 1] = '\0';
+	add_history(buf);
 	if (input_error(input, info))
 		return (get_cmd(buf, buf_size, head, info));
-    free(input);
-    return (0);
+	free(input);
+	return (0);
 }
